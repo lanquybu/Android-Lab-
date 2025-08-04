b2464efbc8566d797f863361f1ffa348240611a3
lanquybu <lanquybu@gmail.com>
2025-08-04 13:04:32 +0700

    Ex7

---
+++ LearnExplicitIntent/app/src/main/java/com/example/learnexplicitintent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.learnexplicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, ChildActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+    }
+}
