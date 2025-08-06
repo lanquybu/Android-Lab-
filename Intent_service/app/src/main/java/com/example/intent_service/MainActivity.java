9cd6ba4762401d14d051f22265f02cacd68e2949
lanquybu <lanquybu@gmail.com>
2025-08-06 10:41:59 +0700

    Ex9

---
+++ Intent_service/app/src/main/java/com/example/intent_service/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.intent_service;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btnplay, btnstop;
+    boolean flag = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnplay = findViewById(R.id.btnplay);
+        btnstop = findViewById(R.id.btnstop);
+
+        btnplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (flag) {
+                    btnplay.setImageResource(R.drawable.pause);
+                    flag = false;
+                } else {
+                    btnplay.setImageResource(R.drawable.play);
+                    flag = true;
+                }
+            }
+        });
+
+        btnstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, MyService.class);
+                stopService(intent2);
+                btnplay.setImageResource(R.drawable.play);
+                flag = true;
+            }
+        });
+    }
+}
---
+++ Intent_service/app/src/main/java/com/example/intent_service/MyService.java
@@ -0,0 +1,39 @@
+package com.example.intent_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    MediaPlayer mymedia;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mymedia = MediaPlayer.create(MyService.this, R.raw.seeu);
+        mymedia.setLooping(true); // Phát lặp lại
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mymedia.isPlaying()) {
+            mymedia.pause();
+        } else {
+            mymedia.start();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mymedia.stop();
+    }
+}
